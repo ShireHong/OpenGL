62df62b680727b32bf036d2d42fd80aa88ad7798
hongzheng2018 <zheng.hong@hannto.com>
2020-03-22 23:29:00 +0800

    修改cmakelist相关的问题

---
+++ my_game/breakout/build/CMakeFiles/breakout.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/git_hub/OpenGL/my_game/breakout/src/breakout.cpp" "D:/git_hub/OpenGL/my_game/breakout/build/CMakeFiles/breakout.dir/src/breakout.cpp.obj"
+  "D:/git_hub/OpenGL/my_game/breakout/src/game.cpp" "D:/git_hub/OpenGL/my_game/breakout/build/CMakeFiles/breakout.dir/src/game.cpp.obj"
+  "D:/git_hub/OpenGL/my_game/breakout/src/resource_manage.cpp" "D:/git_hub/OpenGL/my_game/breakout/build/CMakeFiles/breakout.dir/src/resource_manage.cpp.obj"
+  "D:/git_hub/OpenGL/my_game/breakout/src/shader.cpp" "D:/git_hub/OpenGL/my_game/breakout/build/CMakeFiles/breakout.dir/src/shader.cpp.obj"
+  "D:/git_hub/OpenGL/my_game/breakout/src/texture.cpp" "D:/git_hub/OpenGL/my_game/breakout/build/CMakeFiles/breakout.dir/src/texture.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ my_game/breakout/build/CMakeFiles/breakout.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/breakout.dir/src/breakout.cpp.obj"
+  "CMakeFiles/breakout.dir/src/game.cpp.obj"
+  "CMakeFiles/breakout.dir/src/resource_manage.cpp.obj"
+  "CMakeFiles/breakout.dir/src/shader.cpp.obj"
+  "CMakeFiles/breakout.dir/src/texture.cpp.obj"
+  "breakout.exe"
+  "breakout.exe.manifest"
+  "breakout.pdb"
+  "libbreakout.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/breakout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
